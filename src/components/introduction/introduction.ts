2a415b072cdb1e4b006c821611794df150190d73
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-11-29 14:26:52 +0900

    feat:自己紹介ページ作成

--- src/components/introduction/introduction.ts
+++ src/components/introduction/introduction.ts
@@ -3,6 +3,15 @@ import { transitionPageInfoType } from "@/types";
 
 @Component({})
 export default class Contents extends Vue {
+  textValues: string[] = [
+    "泉 亮輔 (Ryosuke Izumi / is_ryo)",
+    "IoT.kyoto (https://iot.kyoto)",
+    "フロントエンドエンジニア",
+    "Vueにガチハマり中",
+    "(自称)AWS中毒者",
+    "JAWS-UG京都 / Node学園",
+    "Twitter / Qiitadon などに生息"
+  ];
   transitionPageInfo: transitionPageInfoType = {
     preb: "/",
     next: ""
