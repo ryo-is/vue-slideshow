3e3c7a45e6c4cdb2871ad0e9d712a49b5208c0b7
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2018-12-07 11:25:58 +0900

    feat:スライドが変わる方向を矢印で表示するように対応

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -3,11 +3,42 @@ import router from "@/router";
 
 @Component({})
 export default class App extends Vue {
+  prebArrow: boolean = false;
+  nextArrow: boolean = false;
+
   public transitionPage(action: string) {
     const targetRoute: string = this.$parent.$children[0].$children[1].$data
       .transitionPageInfo[action];
     if (targetRoute !== "") {
       router.push(targetRoute);
+    }
+  }
+
+  public showArrow(action: string) {
+    console.log(action);
+    switch (action) {
+      case "preb":
+        this.prebArrow = true;
+        break;
+      case "next":
+        this.nextArrow = true;
+        break;
+      default:
+        break;
+    }
+  }
+
+  public hiddenArrow(action: string) {
+    console.log(action);
+    switch (action) {
+      case "preb":
+        this.prebArrow = false;
+        break;
+      case "next":
+        this.nextArrow = false;
+        break;
+      default:
+        break;
     }
   }
 }
